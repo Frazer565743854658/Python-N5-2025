ce43a7a52f15c879468c8e9299a83e57b2c35a98
Frazer Arbuckle <F.Arbuckle@albynschool.org>
2025-03-10 09:53:57 +0000

    hi

---
+++ select2.py
@@ -0,0 +1,6 @@
+username = input("please enter your username")
+password = input("please enter your username")
+if username == "ian" and password =="letmein":
+    print("Logon accepted")
+else:
+    print("password incorrect")
---
+++ select3.py
@@ -0,0 +1,5 @@
+luckynumber = int(input("Please enter your lucky number: "))
+if luckynumber == 777:
+    print("Ok")
+else:
+    print("keep trying")
