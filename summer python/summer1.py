9840955daee8c44f15c2f9b0da260d4ec6e50496
Frazer Arbuckle <F.Arbuckle@albynschool.org>
2025-09-02 09:57:40 +0100

    hi

---
+++ summer python/summer1.py
@@ -0,0 +1,4 @@
+noWords = int(input("Enter number of words in essay: "))
+noWordsPerMin = int(input("Enter number of words you can type per minute: "))
+noMins = noWords / noWordsPerMin
+print(noMins)
